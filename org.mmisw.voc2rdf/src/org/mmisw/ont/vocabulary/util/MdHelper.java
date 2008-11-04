aa25a22cf19dbc121f64f3e75d5d5e9182908ace
Carlos Rueda <carueda@gmail.com>
2008-11-04 23:28:18 +0000

    2008-11-04 (2.0.alpha3) 	- Integration with ontmd
    	- Everything working pretty nicely! :-)
    	
    	TODO: Unify replicated code with ontmd !!

--- org.mmisw.voc2rdf/src/org/mmisw/ont/vocabulary/util/MdHelper.java
+++ org.mmisw.voc2rdf/src/org/mmisw/ont/vocabulary/util/MdHelper.java
@@ -1,37 +1,29 @@
 package org.mmisw.ont.vocabulary.util;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.mmisw.ont.vocabulary.Omv;
 import org.mmisw.ont.vocabulary.OmvMmi;
 import org.mmisw.voc2rdf.gwt.client.vocabulary.AttrDef;
-import org.mmisw.voc2rdf.gwt.client.vocabulary.AttrGroup;
 import org.mmisw.voc2rdf.gwt.client.vocabulary.Option;
 
-import com.hp.hpl.jena.ontology.OntModel;
-import com.hp.hpl.jena.ontology.Ontology;
-import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.Property;
-import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.vocabulary.DC;
-import com.hp.hpl.jena.vocabulary.OWL;
-
-import edu.drexel.util.rdf.JenaUtil;
-import edu.drexel.util.rdf.OwlModel;
 
 /**
  * Handles the metadata attributes for the ontologies stored in the
  * MMI Registry.
  * 
+ * <p>
+ * TODO Unify similar classes in voc2rdf and ontmd !!
+ * 
  * @author Carlos Rueda
  */
 public class MdHelper {
-	private static String title = "Metadata";
-	
+	private  MdHelper() {}
+
 	// Examples: preferredPrefix(Omv.NS) == "omv";
 	private static Map<String,String> preferredPrefix = new HashMap<String,String>();
 	
@@ -82,343 +74,87 @@ public class MdHelper {
 		return attrDef;
 	}
 	
-	///////////////////////////////////////////////////////////////////////
-	// The metadata groups
-	private static AttrGroup[] attrGroups = {
-		
-		new AttrGroup("General",
-			new AttrDef[] {
-				
-				createAttrDef(Omv.name, true)
-					.setLabel("Title")
-					.setExample("Project Athena Parameters")
-				,
-				createAttrDef(Omv.hasCreator, true)
-					.setLabel("Creator")
-					.setExample("Athena Project")
-				,
-				createAttrDef(Omv.description, true)
-					.setLabel("Description")
-					.setExample("Parameters used in Project Athena")
-				,
-				
-				createAttrDef(Omv.keywords, false)
-					.setLabel("Keywords")
-					.setExample("ocean, physical oceanography, environmental science, climate change")
-				,
+
+	
+	private static AttrDef mainClassAttrDef =
+			createAttrDef(Omv.acronym, true) 
+		    .setLabel("Resource type")
+			.setExample("parameter")
+			//.setAllowUserDefinedOption(true) NO NEED for this in Voc2RDF -- TODO Unify!
+			.addOption(new Option("Discipline", "ISO MD_Keyword: Discipline"))
+			.addOption(new Option("Place", "ISO MD_Keyword: Place"))
+			.addOption(new Option("Stratum", "ISO MD_Keyword: Stratum"))
+			.addOption(new Option("Temporal", "ISO MD_Keyword: Temporal"))
+			.addOption(new Option("Theme", "ISO MD_Keyword: Theme"))
 			
-				createAttrDef(OmvMmi.origVocUri)
-					.setLabel("Link to original vocabulary")
-					.setExample("http://marinemetadata.org/community/teams/athena/parameters.html")
-				,
-
-				createAttrDef(Omv.documentation, false)
-					.setExample("http://marinemetadata.org/community/teams/athena")
-					.setLabel("Documentation URL")
-				,
-		
-				createAttrDef(OmvMmi.origMaintainerCode, true)
-					.setLabel("Authority abbreviation")
-					.setExample("mmi")
-					.addOption(new Option("--choose--", "--choose one--"))
-					.addOption(new Option("cencoos", "cencoos: Central California Ocean Observing System"))
-					.addOption(new Option("gcoos", "gcoos: Gulf of Mexico Ocean Observing System"))
-					.addOption(new Option("mmi", "mmi: Marine Metadata Interoperability"))
-					.addOption(new Option("oi2008demo", "oi2008demo: Oceans Innovations 2008 Demonstration"))
-					.addOption(new Option("q2o", "q2o: QARTOD-to-OGC Project"))
-//					.addOption(new Option("argo", "argo: Argo Project"))
-//					.addOption(new Option("gcmd", "gcmd: Global Change Master Directory"))
-//					.addOption(new Option("cf", "cf: Climate and Forecast Conventions Standards Names"))
-					.addOption(new Option("*", "--other, please specify"))
-				, 
-				
-//				createAttrDef(Omv.hasDomain),
-				
-				createAttrDef(Omv.creationDate)
-					.setLabel("Creation date")
-					.setInternal(true)
-					.setExample(org.mmi.util.ISO8601Date.getCurrentDateBasicFormat())
-				,
-				
-				
-				createAttrDef(Omv.acronym, true) 
-					.setLabel("Main theme")
-					.setExample("parameter")
-					.addOption(new Option("parameter", "Parameters (It will include terms like 'sea surface salinity')"))
-					.addOption(new Option("sensorType", "Sensor types (It will include terms like 'Thermometer')"))
-					.addOption(new Option("platformType", "Platform types (It will include terms like 'Mooring')"))
-					.addOption(new Option("unit", "Units  (It will include terms like 'meter')"))
-					.addOption(new Option("keyword", "Keywords  (It will include terms like 'climate', 'oceans')"))
-					.addOption(new Option("organization", "Organizations  (It will include terms like 'MBARI' or 'MMI')"))
-					.addOption(new Option("process", "Processes  (It will include terms like 'data quality control')"))
-					.addOption(new Option("missingflag", "Missing flags  (It will include terms like '-999')"))
-					.addOption(new Option("qualityflag", "Quality flags  (It will include terms like '10')"))
-					.addOption(new Option("featureType", "Feature types  (It will include terms like 'body of water')"))
-					.addOption(new Option("GeographicFeature", "Geographic features  (It will include terms like 'Monterey Bay')"))
-				, 
-
-				
-				//createAttrDef(DC.publisher),
-				
-				createAttrDef(Omv.hasContributor)
-					.setLabel("Contributor(s)")
-					.setExample("Jane Collaborator, Joe Vocabulary Manager, Fido the Mascot")
-				,
-				
-				createAttrDef(Omv.uri)
-					.setInternal(true)
-				,
-				
-				
-				// TODO createAttrDef(Omv.hasPriorVersion),
-				
-				
-				createAttrDef(OmvMmi.shortNameUri)
-					.setLabel("URI of ontology topic")
-					.setExample("http://mmisw.org/ont/mmi/topicTheme/parameter")
-				,
-				
-				
-//				createAttrDef(DC.coverage),
-//				createAttrDef(DC.format),
-//				createAttrDef(DC.language),
-//				createAttrDef(DC.relation),
-//				createAttrDef(DC.rights),
-//				createAttrDef(DC.type),
-
-			}
-		),
-		new AttrGroup("Usage/License/Permissions",
-			new AttrDef[] {
-				
-				createAttrDef(OmvMmi.origVocManager)
-					.setLabel("Manager of source vocabulary")
-					.setExample("Athena Project")
-				,
-				
-				createAttrDef(OmvMmi.contact)
-					.setLabel("Contact/Responsible Party")
-					.setExample("Joe Vocabulary Manager")
-				,
-				createAttrDef(OmvMmi.contactRole)
-					.setLabel("Contact role")
-					.setExample("content manager")
-					.addOption(new Option("--choose one--"))
-					.addOption(new Option("content manager"))
-					.addOption(new Option("ontology producer"))
-					.addOption(new Option("organizational manager"))
-					.addOption(new Option("IP negotiator"))
-					.addOption(new Option("other"))
-				,
-				
-				// TODO createAttrDef(OmvMmi.accessStatus),
-				
-				// TODO createAttrDef(OmvMmi.accessStatusDate),
-
-				// TODO createAttrDef(OmvMmi.licenseCode),
-				// TODO createAttrDef(OmvMmi.licenseReference),
-				// TODO createAttrDef(OmvMmi.licenseAsOfDate),
-				
-				createAttrDef(OmvMmi.temporaryMmiRole)
-					.setLabel("Temporary MMI role")
-					.setExample("ontology producer")
-					.addOption(new Option("--choose one--"))
-					.addOption(new Option("content manager"))
-					.addOption(new Option("ontology producer"))
-					.addOption(new Option("ontology republisher"))
-					.addOption(new Option("other"))
-				,
-				
-				// TODO createAttrDef(OmvMmi.agreedMmiRole),
-				
-				createAttrDef(OmvMmi.creditRequired)
-					.setExample("yes")
-					.setLabel("Author credit required")
-					.addOption(new Option("not specified"))
-					.addOption(new Option("yes"))
-					.addOption(new Option("no"))
-					.addOption(new Option("conditional"))
-				,
-				
-				// TODO createAttrDef(OmvMmi.creditConditions),
-				
-				createAttrDef(OmvMmi.creditCitation)
-					.setLabel("Citation string")
-					.setExample("Vocabulary provided by Athena Project")
-				,
-			}
-		),
-		new AttrGroup("Original source",
-			new AttrDef[] {
-				
-				createAttrDef(OmvMmi.origVocDocumentationUri)
-					.setLabel("URI of original vocabulary")
-				,
-				
-				createAttrDef(OmvMmi.origVocDescriptiveName)
-					.setLabel("Descriptive name")
-				,
-				
-				createAttrDef(OmvMmi.origVocVersionId)
-					.setLabel("Version")
-				,
-				
-				createAttrDef(OmvMmi.origVocKeywords)
-					.setLabel("Keywords")
-				,
-				
-				createAttrDef(OmvMmi.origVocSyntaxFormat)
-					.setLabel("Syntax format")
-				,
-			}
-		),
-	};
-
-	
-	public static AttrGroup[] getAttrGroups() {
-		return attrGroups;
-	}
-	
-	
-	private static AttrDef[] attrDefs = {
-		
-	};
-	
-	private static Map<String,AttributeValue> _initAttributes(Map<String,AttributeValue> attributes) {
-		for ( AttrDef attrDef : attrDefs ) {
-			attributes.put(attrDef.getLocalName(), new AttributeValue(attrDef));
-		}
-		return attributes;	
-	}
-	
-	/**
-	 * attributes that can/should be associated
-	 */
-	private Map<String,AttributeValue> attributes;
-	
-	
-	/** 
-	 * Creates an ontology metadata helper.
-	 */
-	public MdHelper() {
-		attributes = _initAttributes(new LinkedHashMap<String,AttributeValue>());	
-	}
-	
-	public String getTitle() {
-		return title;
-	}
-
-	/** 
-	 * Gets all the attributes
-	 * @return All the attributes
-	 */
-	public Collection<AttributeValue> getAttributes() {
-		return attributes.values();
-	}
-	
-	
-	/**
-	 * Updates the given model with the non-empty-valued attributes in this
-	 * object.
-	 * 
-	 * @param ontModel The model to be updated.
-	 */
-	public void updateModel(OntModel ontModel) {
-
-		// see that at least one attribute has a value associated
-		boolean hasValues = false;
-		for ( AttributeValue attr : getAttributes() ) {
-			String values = attr.getValue();
-			if ( values.length() > 0 ) {
-				hasValues = true;
-				break;
-			}
-		}
-		
-		if ( !hasValues ) {
-			System.out.println(this.getClass().getName()+ ": no metadata to update in model...");
-			return;
-		}
-		
-		System.out.println(this.getClass().getName()+ ": updating metadata in model ...");
-		OwlModel newOntModel = new OwlModel(ontModel);
-		Ontology ontolgy = newOntModel.createOntology(JenaUtil.getURIForBase(""));
-		
-		for ( AttributeValue attr : getAttributes() ) {
-			String value = attr.getValue();
-			if ( value.length() > 0 ) {
-				ontolgy.addProperty(uriPropMap.get(attr.attrDef.getUri()), value);
-			}
-		}
-	}
-
-	
-	/**
-	 * Updates the attributes in this object using the metadata in the 
-	 * given model.
-	 * 
-	 * @param model The model to read metadata from.
-	 */
-	public void updateAttributesFromModel(Model ontModel) {
-
-		System.out.println(this.getClass().getName()+ ": updating attributes with model metadata ...");
-		
-		Resource ontRes = JenaUtil.getFirstIndividual(ontModel, OWL.Ontology);
-		
-		if ( ontRes == null ) {
-			return;
-		}
-		
-		for ( AttrDef attrDef : attrDefs ) {
-			Property dcProp = uriPropMap.get(attrDef.getUri());
-			String value = JenaUtil.getValue(ontRes, dcProp);
-			if (value == null) {
-				continue;
-			}
-			//	value = JenaUtil.getBaseURI(ontModel);
+			.addOption(new Option("axis", "OGC Object Type: axis"))
+			.addOption(new Option("axisDirection", "OGC Object Type: axisDirection"))
+			.addOption(new Option("coordinateOperation", "OGC Object Type: coordinateOperation"))
+			.addOption(new Option("crs", "OGC Object Type: crs"))
+			.addOption(new Option("cs", "OGC Object Type: cs"))
+			.addOption(new Option("datum", "OGC Object Type: datum"))
+			.addOption(new Option("dataType", "OGC Object Type: dataType"))
+			.addOption(new Option("derivedCRSType", "OGC Object Type: derivedCRSType"))
+			.addOption(new Option("documentType", "OGC Object Type: documentType"))
+			.addOption(new Option("ellipsoid", "OGC Object Type: ellipsoid"))
+			.addOption(new Option("featureType", "OGC Object Type: featureType"))
+			.addOption(new Option("group", "OGC Object Type: group"))
+			.addOption(new Option("meaning", "OGC Object Type: meaning"))
+			.addOption(new Option("meridian", "OGC Object Type: meridian"))
+			.addOption(new Option("method", "OGC Object Type: method"))
+			.addOption(new Option("nil", "OGC Object Type: nil"))
+			.addOption(new Option("parameter", "OGC Object Type: parameter"))
+			.addOption(new Option("phenomenon", "OGC Object Type: phenomenon"))
+			.addOption(new Option("pixelInCell", "OGC Object Type: pixelInCell"))
+			.addOption(new Option("rangeMeaning", "OGC Object Type: rangeMeaning"))
+			.addOption(new Option("referenceSystem", "OGC Object Type: referenceSystem"))
+			.addOption(new Option("uom", "OGC Object Type: uom"))
+			.addOption(new Option("verticalDatumType", "OGC Object Type: verticalDatumType"))
 			
-			AttributeValue attr = attributes.get(dcProp.getLocalName());
-			if ( attr != null ) {
-				attr.setValue(value);
-			}
-			
-		}
-		System.out.println();
-	}
-
+			.addOption(new Option("keyword", "Other: keyword"))
+			.addOption(new Option("parameter", "Other: parameter"))
+			.addOption(new Option("unit", "Other: unit"))
+			.addOption(new Option("organization", "Other: organization"))
+			.addOption(new Option("platform", "Other: platform"))
+			.addOption(new Option("sensor", "Other: sensor"))
+			.addOption(new Option("process", "Other: process"))
+			.addOption(new Option("missingFlag", "Other: missingFlag"))
+			.addOption(new Option("qualityFlag", "Other: qualityFlag"))
+			.addOption(new Option("qcCategory", "Other: qcCategory"))
+			.addOption(new Option("coordinateReference", "Other: coordinateReference"))
+			.addOption(new Option("datum", "Other: datum"))
+			.addOption(new Option("protocol", "Other: protocol"))
+			.addOption(new Option("metadataStandard", "Other: metadataStandard"))
+			.addOption(new Option("featureType", "Other: featureType"))
+			.addOption(new Option("featureName", "Other: featureName"))
+			.addOption(new Option("speciesType", "Other: speciesType"))
+			.addOption(new Option("speciesName", "Other: speciesName"))
+			.addOption(new Option("discipline", "Other: discipline"))
+			.addOption(new Option("place", "Other: place"))
+			.addOption(new Option("theme", "Other: theme"))
+			.addOption(new Option("roleOfContact", "Other: roleOfContact"))
+			.addOption(new Option("general", "Other: general metadata attribute"))
+		;
 	
-	/**
-	 * A metadata attribute with associated value. 
-	 */
-	public static class AttributeValue {
-		private AttrDef attrDef;
-
-		private String value = "";
-		
-		public AttributeValue(AttrDef attrDef) {
-			this.attrDef = attrDef;
-		}
-
-		public String getNamespace() {
-			return attrDef.getNameSpace();
-		}
-		
-		public String getLabel() {
-			return attrDef.getLocalName();
-		}
-		
-		public String getValue() {
-			return value;
-		}
-		
-		public void setValue(String value) {
-			this.value = value.trim();
-		}
-		
-		public Property getProperty() {
-			return uriPropMap.get(attrDef.getUri());
-		}
-	}
+//   OLD list
+//			createAttrDef(Omv.acronym, true) 
+//			.setLabel("Main theme")
+//			.setExample("parameter")
+//			.addOption(new Option("parameter", "Parameters (It will include terms like 'sea surface salinity')"))
+//			.addOption(new Option("sensorType", "Sensor types (It will include terms like 'Thermometer')"))
+//			.addOption(new Option("platformType", "Platform types (It will include terms like 'Mooring')"))
+//			.addOption(new Option("unit", "Units  (It will include terms like 'meter')"))
+//			.addOption(new Option("keyword", "Keywords  (It will include terms like 'climate', 'oceans')"))
+//			.addOption(new Option("organization", "Organizations  (It will include terms like 'MBARI' or 'MMI')"))
+//			.addOption(new Option("process", "Processes  (It will include terms like 'data quality control')"))
+//			.addOption(new Option("missingflag", "Missing flags  (It will include terms like '-999')"))
+//			.addOption(new Option("qualityflag", "Quality flags  (It will include terms like '10')"))
+//			.addOption(new Option("featureType", "Feature types  (It will include terms like 'body of water')"))
+//			.addOption(new Option("GeographicFeature", "Geographic features  (It will include terms like 'Monterey Bay')"))
+//			;
 	
-
+	
+	public static AttrDef getMainClassAttrDef() {
+		return mainClassAttrDef;
+	}
 }
