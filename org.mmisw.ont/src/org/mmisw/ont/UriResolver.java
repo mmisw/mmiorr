b6bd190863669ac5c2a14e5c2eff499f838c1af4
Carlos Rueda <carueda@gmail.com>
2008-11-15 01:30:01 +0000

    Issue 53: unversioned reponse wrong for "slash" ontologies 	UnversionedConverter: Using JenaUtil2 to handle the fragments as in OntMd 	(TODO JenaUtil2.java is a copy of the same file on ontmd -> unify in a libray)

--- org.mmisw.ont/src/org/mmisw/ont/UriResolver.java
+++ org.mmisw.ont/src/org/mmisw/ont/UriResolver.java
@@ -652,7 +652,7 @@ public class UriResolver extends HttpServlet {
 	 */
 	private StringReader _serializeModel(Model model, String lang) {
 		StringWriter sw = new StringWriter();
-		String base = JenaUtil.getURIForBase(model.getNsPrefixURI(""));
+		String base = JenaUtil2.getURIForBase(model.getNsPrefixURI(""));
 		RDFWriter writer = model.getWriter(lang);
 		writer.setProperty("xmlbase", base);
 		writer.setProperty("showXmlDeclaration", "true");
