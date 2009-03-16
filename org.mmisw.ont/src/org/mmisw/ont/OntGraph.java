d3193e26937a48bb788871feb9445e24d68ed86b
Carlos Rueda <carueda@gmail.com>
2009-03-16 18:21:07 +0000

    preparations for loading "unversioned" latest versions into the graph

--- org.mmisw.ont/src/org/mmisw/ont/OntGraph.java
+++ org.mmisw.ont/src/org/mmisw/ont/OntGraph.java
@@ -1,5 +1,6 @@
 package org.mmisw.ont;
 
+import java.net.URISyntaxException;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -7,25 +8,36 @@ import javax.servlet.ServletException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.StmtIterator;
 import com.hp.hpl.jena.util.PrintUtil;
 
+import edu.drexel.util.rdf.JenaUtil;
+
 
 /**
- * A helper to handle the "big" graph of all existing ontologies.
+ * Handles the "big" graph of all existing ontologies as returned by
+ * {@link org.mmisw.ont.Db#getOntologies()}.
  * 
  * @author Carlos Rueda
  */
 public class OntGraph {
 	
 	private final Log log = LogFactory.getLog(OntGraph.class);
+	
+	/** Load the unversioned form of the ontologies? 
+	 * I'm using this flag to prepare/test the changes.
+	 * Set to false to get the original behavior (load "versioned" ontologies).
+	 */
+	private static final boolean USE_UNVERSIONED = true;
 
 	private final OntConfig ontConfig;
 	private final Db db;
 	
+	/** the model with all the ontologies */
 	private Model _model;
 	
 	private String aquaUploadsDir;
@@ -33,7 +45,10 @@ public class OntGraph {
 	
 	/**
 	 * Creates an instance of this helper.
-	 * @param ontConfig Used at initialization.
+	 * 
+	 * @param ontConfig Used at initialization to obtain the "uploads" directory, where the
+	 *        actual ontology files are located.
+	 *        
 	 * @param db The database helper.
 	 */
 	OntGraph(OntConfig ontConfig, Db db) {
@@ -77,27 +92,48 @@ public class OntGraph {
 	
 	private void _doInitModel() throws ServletException {
 		_model = ModelFactory.createDefaultModel();
+		
+		// get the list of (latest-version) ontologies:
 		List<Ontology> onts = db.getOntologies();
+		
+		if ( log.isDebugEnabled() ) {
+			log.debug("Using unversioned ontologies: " +USE_UNVERSIONED);
+		}
+		
+		// 
 		for ( Ontology ontology : onts ) {
 			String full_path = aquaUploadsDir 
 				+ "/" +ontology.file_path + "/" + ontology.filename;
 		
 			log.info("init: loading: " +full_path);
-			String absPath = "file:" + full_path;
 			
-			try {
 				
-				_model.read(absPath, "", null);
-				
-//				// TODO processImports: true or false?
-//				boolean processImports = false;
-//				Model model_ = JenaUtil.loadModel(absPath, processImports);
-//				_model.add(model_);
-				
-				log.info("added " + absPath + " to model");
-			} 
-			catch (Exception e) {
-				log.error("Unable to add " + absPath + " to model");
+			if ( USE_UNVERSIONED ) {
+				OntModel model = JenaUtil.loadModel(full_path, false);
+				MmiUri mmiUri;
+				try {
+					mmiUri = new MmiUri(ontology.getUri());
+				}
+				catch (URISyntaxException e) {
+					log.error("shouldn't happen", e);
+					continue;
+				}
+				OntModel unversionedModel = UnversionedConverter.getUnversionedModel(model, mmiUri);
+				_model.add(unversionedModel);
+			}
+			else {
+				String absPath = "file:" + full_path;
+				try {
+					_model.read(absPath, "", null);
+					
+					// TODO processImports: true or false?
+//					boolean processImports = false;
+//					Model model_ = JenaUtil.loadModel(absPath, processImports);
+//					_model.add(model_);
+				} 
+				catch (Exception e) {
+					log.error("Unable to add " + absPath + " to model");
+				}
 			}
 		}
 
