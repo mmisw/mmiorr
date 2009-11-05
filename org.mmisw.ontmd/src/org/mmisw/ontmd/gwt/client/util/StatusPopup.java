67839f85a7d4180fa3cb4775646fad52e9f2de7d
Carlos Rueda <carueda@gmail.com>
2009-11-05 02:44:02 +0000

    2009-11-04   1.6.1.alpha 	Implemented #203: Allow to upload file for new version instead of in-place editing 	- PortalMainPanel.editNewVersion: now a confirmation dialog is shown so the user can 	  decide whether s/he wants in-place editing of the contents of the ontology (and the metadata 	  as well), OR upload a file for the new version. The message is:
    		Do you want to do in-place editing of the contents of the ontology?
    		Click OK to proceed with in-place editing of the contents and the metadata.
    		Click Cancel to proceed with file upload and metadata editing (file upload is optional).
    		
    	Fixed #186: "progress info remains open after task in completed"
    	- StatusPopup: now has an internal timer to auto-hide the popup every few seconds. The popup will
    	  show up again upon any update to the status. See StatusPopup for more details.

--- org.mmisw.ontmd/src/org/mmisw/ontmd/gwt/client/util/StatusPopup.java
+++ org.mmisw.ontmd/src/org/mmisw/ontmd/gwt/client/util/StatusPopup.java
@@ -1,10 +1,25 @@
 package org.mmisw.ontmd.gwt.client.util;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.DeferredCommand;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.PopupPanel;
 
 /**
+ * A simple popup window to show progress information.
+ * 
+ * <p>
+ * It has an internal timer so the popup is automatically hidden every
+ * few seconds but displayed again upon any update to the status.
+ * This mechanism helps deal with cases where long running tasks (via
+ * IncrementalCommand's, for example) fail to call the finalization part where normally an
+ * explicit call to hide() will be done.
+ * This was a quick fix to 
+ * <a href="http://code.google.com/p/mmisw/issues/detail?id=186">issue 186</a>.
+ * <p>
+ * TODO a more robust mechanism in general.
  * 
  * @author Carlos Rueda
  */
@@ -16,6 +31,18 @@ public class StatusPopup {
 	
 	private final HTML statusHtml = new HTML();
 	
+	private boolean showCalled;
+	private boolean hideCalled;
+	
+	private final Timer autoHideTimer = new Timer() {
+		public void run() {
+			popup.hide();
+			if ( hideCalled ) {
+				autoHideTimer.cancel();
+			}
+		}
+	};
+	
 	public StatusPopup(String width, boolean modal) {
 		statusHtml.setWidth(width);
 		popup = new PopupPanel(false, modal);
@@ -25,21 +52,37 @@ public class StatusPopup {
 	public void show(int left, int top) {
 		popup.setPopupPosition(left, top);
 		popup.show();
+		showCalled = true;
+		hideCalled = false;
+		autoHideTimer.scheduleRepeating(2*2000);
 	}
 	
 	public void setStatus(String msg) {
 		String html = useProgressAnimation ? "<img src=\"" +GWT.getModuleBaseURL()+ "images/loading.gif\"> " : "" ;
 		html += "<font color=\"blue\"><i>" +msg+ "</i></font>";
 		
+		show();
+
 		statusHtml.setHTML(msg);
 	}
-	
+
+	private void show() {
+		if ( showCalled && ! hideCalled ) {
+			DeferredCommand.addCommand(new Command() {
+				public void execute() {
+					popup.show();
+				}
+			});
+		}
+	}
+
 	public void setStatusHtml(String html) {
+		show();
 		statusHtml.setHTML(html);
 	}
 	
 	public void hide() {
-		statusHtml.setHTML("");
+		hideCalled = true;
 		popup.hide();
 	}
 
