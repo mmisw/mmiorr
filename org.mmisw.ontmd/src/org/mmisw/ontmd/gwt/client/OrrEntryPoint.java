35a209c88c69996150e7db951dc5f58d6df668e8
Carlos Rueda <carueda@gmail.com>
2010-09-03 04:44:58 +0000

    more clean-up; module now named Orr (Orr.gwt.xml); Main ranamed to OrrEntryPoint

---
+++ org.mmisw.ontmd/src/org/mmisw/ontmd/gwt/client/OrrEntryPoint.java
@@ -0,0 +1,38 @@
+package org.mmisw.ontmd.gwt.client;
+
+import java.util.Map;
+
+import org.mmisw.ontmd.gwt.client.util.OrrUtil;
+
+import com.google.gwt.core.client.EntryPoint;
+
+
+/**
+ * The entry point for the ORR Portal application.
+ * 
+ * @author Carlos Rueda
+ */
+public class OrrEntryPoint implements EntryPoint {
+
+	/**
+	 * This is the entry point for the ORR Portal application.
+	 */
+	public void onModuleLoad() {
+		boolean withLogging = false;
+		
+		Map<String, String> params = OrrUtil.getParams();
+
+		if (params != null) {
+			String _log = (String) params.get("_log");
+			if (_log != null) {
+				withLogging = true;
+				params.remove("_log");
+			}
+		}
+		
+		Orr.init(withLogging);
+		
+		Orr.launch(params);
+	}
+	
+}
