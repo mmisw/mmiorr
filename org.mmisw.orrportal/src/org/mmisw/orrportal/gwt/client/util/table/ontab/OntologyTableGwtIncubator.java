718569c223560b78702561d0536843e20e81f1e6
Carlos Rueda <carueda@gmail.com>
2010-11-17 07:39:08 +0000

    GWT 2.x: Not significant outcome from new attempt: Development mode continues to be intolerably slow (perhaps because of the mix between old and new underlying GWT mechanisms?), and the build process continues to be complicated, in fact, I couldn't make 2.1 to run properly because of an error related with SmartGWT, which does not happen with 2.0.4). So, reverting to the previous state, except for the change in OrrService above and the removal of the dependency on module com.google.gwt.gen2.Gen2 (ie., gwt-incubator_1-5_Dec_28.jar) as the code for this is not activated and will still need a more stable basis in general.

--- org.mmisw.orrportal/src/org/mmisw/orrportal/gwt/client/util/table/ontab/OntologyTableGwtIncubator.java
+++ org.mmisw.orrportal/src/org/mmisw/orrportal/gwt/client/util/table/ontab/OntologyTableGwtIncubator.java
@@ -6,18 +6,21 @@ import org.mmisw.orrclient.gwt.client.rpc.LoginResult;
 import org.mmisw.orrclient.gwt.client.rpc.RegisteredOntologyInfo;
 import org.mmisw.orrportal.gwt.client.util.table.IQuickInfo;
 
-import com.google.gwt.gen2.table.client.FixedWidthFlexTable;
-import com.google.gwt.gen2.table.client.FixedWidthGrid;
-import com.google.gwt.gen2.table.client.ScrollTable;
-import com.google.gwt.gen2.table.client.SelectionGrid.SelectionPolicy;
-import com.google.gwt.gen2.table.override.client.FlexTable.FlexCellFormatter;
-import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+//import com.google.gwt.gen2.table.client.FixedWidthFlexTable;
+//import com.google.gwt.gen2.table.client.FixedWidthGrid;
+//import com.google.gwt.gen2.table.client.ScrollTable;
+//import com.google.gwt.gen2.table.client.SelectionGrid.SelectionPolicy;
+//import com.google.gwt.gen2.table.override.client.FlexTable.FlexCellFormatter;
+//import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
  * Possible implementation based on GWT incubator.
  * This is only a partial implementation. Needs improvement for when time permits.
  * 
+ * <p>
+ * NOTE: all actual code commented -- DO NOT USE.
+ *
  * @author Carlos Rueda
  */
 public class OntologyTableGwtIncubator extends BaseOntologyTable {
@@ -29,27 +32,29 @@ public class OntologyTableGwtIncubator extends BaseOntologyTable {
 	 */
 	public OntologyTableGwtIncubator(IQuickInfo quickInfo, boolean isVersionsTable) {
 		super(quickInfo, isVersionsTable);
+		throw new UnsupportedOperationException();
 		
-		FixedWidthFlexTable headerTable = createHeaderTable();
-		dataTable = _createDataTable();
-
-		// Combine the components into a ScrollTable
-		scrollTable = new ScrollTable(dataTable, headerTable);
-
-		// Set some options in the scroll table
-//		scrollTable.setSortingEnabled(true);
-		scrollTable.setResizePolicy(ScrollTable.ResizePolicy.FILL_WIDTH);
-		scrollTable.setSize("100%", "100%");
+//		FixedWidthFlexTable headerTable = createHeaderTable();
+//		dataTable = _createDataTable();
+//
+//		// Combine the components into a ScrollTable
+//		scrollTable = new ScrollTable(dataTable, headerTable);
+//
+//		// Set some options in the scroll table
+////		scrollTable.setSortingEnabled(true);
+//		scrollTable.setResizePolicy(ScrollTable.ResizePolicy.FILL_WIDTH);
+//		scrollTable.setSize("100%", "100%");
 	}
 
 	public void clear() {
 		// TODO 
-		dataTable.resize(0, 4);
+//		dataTable.resize(0, 4);
 
 	}
 
 	public Widget getWidget() {
-		return scrollTable;
+//		return scrollTable;
+		return null;
 //		return xx.getWidget();
 	}
 
@@ -57,25 +62,25 @@ public class OntologyTableGwtIncubator extends BaseOntologyTable {
 			LoginResult loginResult
 	) {
 
-		dataTable.resize(ontologyInfos.size(), 4);
-
-		int row = 0;
-		for (RegisteredOntologyInfo oi : ontologyInfos) {
-		
-			String[] cols = { _getUri(oi), _getName(oi), _getAuthor(oi), _getVersion(oi) };
-			
-			for ( int col = 0; col < cols.length; col++ ) {
-				dataTable.setHTML(row, col, cols[col]);
-				dataTable.getCellFormatter().setWordWrap(row, col, true);
-			}
-
-			row++;
-		}
-		
+//		dataTable.resize(ontologyInfos.size(), 4);
+//
+//		int row = 0;
+//		for (RegisteredOntologyInfo oi : ontologyInfos) {
+//		
+//			String[] cols = { _getUri(oi), _getName(oi), _getAuthor(oi), _getVersion(oi) };
+//			
+//			for ( int col = 0; col < cols.length; col++ ) {
+//				dataTable.setHTML(row, col, cols[col]);
+//				dataTable.getCellFormatter().setWordWrap(row, col, true);
+//			}
+//
+//			row++;
+//		}
+//		
 //		dataTable.setColumnWidth(0, 500);
 //		dataTable.setColumnWidth(1, 300);
 		
-		dataTable.sortColumn(3, false);
+//		dataTable.sortColumn(3, false);
 
 	}
 
@@ -85,8 +90,8 @@ public class OntologyTableGwtIncubator extends BaseOntologyTable {
 
 	public void showProgress() {
 		// TODO 
-		dataTable.resize(1, 1);
-		dataTable.setHTML(0, 0, "<i>one moment...</i>");
+//		dataTable.resize(1, 1);
+//		dataTable.setHTML(0, 0, "<i>one moment...</i>");
 
 	}
 
@@ -99,42 +104,42 @@ public class OntologyTableGwtIncubator extends BaseOntologyTable {
 	 * 
 	 * @return a data table
 	 */
-	private FixedWidthGrid _createDataTable() {
-		FixedWidthGrid dataTable = new FixedWidthGrid();
-		dataTable.setSelectionPolicy(SelectionPolicy.ONE_ROW);
-		return dataTable;
-	}
+//	private FixedWidthGrid _createDataTable() {
+//		FixedWidthGrid dataTable = new FixedWidthGrid();
+//		dataTable.setSelectionPolicy(SelectionPolicy.ONE_ROW);
+//		return dataTable;
+//	}
 
 	
-	/**
-	 * Create the header table.
-	 * 
-	 * @return a header table
-	 */
-	private FixedWidthFlexTable createHeaderTable() {
-		// Create a new table
-		FixedWidthFlexTable headerTable = new FixedWidthFlexTable();
-		FlexCellFormatter formatter = headerTable.getFlexCellFormatter();
-
-		final int row = 0;
-		headerTable.setHTML(row, 0, "URI");
-		formatter.setHorizontalAlignment(row, 0,
-				HasHorizontalAlignment.ALIGN_CENTER);
-		headerTable.setHTML(row, 1, "Name");
-		formatter.setHorizontalAlignment(row, 1,
-				HasHorizontalAlignment.ALIGN_CENTER);
-		headerTable.setHTML(row, 2, "Author");
-		formatter.setHorizontalAlignment(row, 2,
-				HasHorizontalAlignment.ALIGN_CENTER);
-		headerTable.setHTML(row, 3, "Version");
-		formatter.setHorizontalAlignment(row, 3,
-				HasHorizontalAlignment.ALIGN_CENTER);
-
-		return headerTable;
-	}
+//	/**
+//	 * Create the header table.
+//	 * 
+//	 * @return a header table
+//	 */
+//	private FixedWidthFlexTable createHeaderTable() {
+//		// Create a new table
+//		FixedWidthFlexTable headerTable = new FixedWidthFlexTable();
+//		FlexCellFormatter formatter = headerTable.getFlexCellFormatter();
+//
+//		final int row = 0;
+//		headerTable.setHTML(row, 0, "URI");
+//		formatter.setHorizontalAlignment(row, 0,
+//				HasHorizontalAlignment.ALIGN_CENTER);
+//		headerTable.setHTML(row, 1, "Name");
+//		formatter.setHorizontalAlignment(row, 1,
+//				HasHorizontalAlignment.ALIGN_CENTER);
+//		headerTable.setHTML(row, 2, "Author");
+//		formatter.setHorizontalAlignment(row, 2,
+//				HasHorizontalAlignment.ALIGN_CENTER);
+//		headerTable.setHTML(row, 3, "Version");
+//		formatter.setHorizontalAlignment(row, 3,
+//				HasHorizontalAlignment.ALIGN_CENTER);
+//
+//		return headerTable;
+//	}
 
 	
-	private ScrollTable scrollTable;
-	private FixedWidthGrid dataTable;
+//	private ScrollTable scrollTable;
+//	private FixedWidthGrid dataTable;
 
 }
