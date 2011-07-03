7bfeee8849ab20c274b86cc2f60a5168b0353dad
Carlos Rueda <carueda@gmail.com>
2011-07-03 04:55:21 +0000

    initial version

--- misc/js/orr-ionrole-showtooltip.js
+++ misc/js/orr-ionrole-showtooltip.js
@@ -1,56 +1,5 @@
-## @param0 : Some string to differentiate multiple occurrences of the same term in the same confluence page
 
-#if ($param0)
-	#set ($anchor = $param0)
-#else
-	#set ($anchor = "")
-#end
-#set ($IID = $body + $anchor)
-
-<script src="http://cdn.jquerytools.org/1.2.5/full/jquery.tools.min.js"></script>
-
-<script src="https://mmisw.googlecode.com/svn/trunk/misc/js/orr-jquery-csv.js"></script>
-
-<link rel="stylesheet" type="text/css" href="https://mmisw.googlecode.com/svn/trunk/misc/css/orr-tooltip.css"/>
-
-
-<script>
-var ionroleprefix = "http://mmisw.org/ont/ooi/ionrole/";
-if (! ionroledict) {
-	var ionroledict = {};
-}
-ionroledict['$IID'] = {};
-ionroledict['$IID']['term'] = "$body".trim();
-ionroledict['$IID']['termId'] = ionroledict['$IID']['term'].replace(/ /g, '_');
-ionroledict['$IID']['uri'] = ionroleprefix + ionroledict['$IID']['termId'];
-
-</script>
-
-
-<a id="mytable$IID" href="">$body</a>
-<div id="tooltip$IID" class="tooltip">
-	<table border="0" width="500px">
-		<tr>
-			<td class="label">URI</td>
-			<td class="value">
-			   <a id="uri$IID" target="_blank" href="">$body</a>
-			</td>
-		</tr>
-		<tr>
-			<td class="label">Name</td>
-			<td class="value" id="name$IID"> <i>loading...</i> </td>
-		</tr>
-		<tr>
-			<td class="label">Description</td>
-			<td class="value" id="description$IID"> <i>loading...</i></td>
-
-		</tr>
-	</table>
-</div>
-
-<script>
-
-		function showTooltip(tableId, aid) {
+		function ionrole_showTooltip(tableId, aid) {
 			var anchorId = aid;
 			resultElem = document.getElementById(tableId);
 			if ( resultElem.data != null ) {
@@ -99,24 +48,4 @@ ionroledict['$IID']['uri'] = ionroleprefix + ionroledict['$IID']['termId'];
 			query = query.replace(/ /g, '+');
 			loadInfo("http://mmisw.org/ont?form=csv&sparql=" +query);
 		}
-
-
-// execute your scripts when the DOM is ready. this is a good habit
-$(function() {
-		
-	$("#mytable$IID").tooltip({
-		//tip: '#tooltip$IID',
-		position: 'bottom right',
-		delay: 200,
-		effect: 'fade',
-		relative: 'true',
-		
-		onShow: function() {
-			showTooltip("mytable$IID", "$IID");
-		}
-		
-	});
-});
-	
-</script>
 
