51fa9450e9bbf13af364565425e20020f0bd24a7
Carlos Rueda <carueda@gmail.com>
2011-07-05 23:06:03 +0000

    more 4 IE

--- misc/js/orr-ionrole.js
+++ misc/js/orr-ionrole.js
@@ -35,8 +35,7 @@ function IonRoleTooltip(term, anchorId) {
 				}
 				else {// code for IE6, IE5
 					xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-					//xmlhttp = new ActiveXObject("MSXML2.XMLHTTP.3.0");
-          xmlhttp.setRequestHeader("Origin", "https://confluence-dev.oceanobservatories.org");
+					//xmlhttp = new ActiveXObject("MSXML2.XMLHTTP.3.0"););
 				}
 				xmlhttp.onreadystatechange=function() {
 					if (xmlhttp.readyState==4 ) {
@@ -53,6 +52,7 @@ function IonRoleTooltip(term, anchorId) {
 					}
 				}
 				xmlhttp.open("GET", url, true);
+        xmlhttp.setRequestHeader("Origin", "https://confluence-dev.oceanobservatories.org");
 				xmlhttp.send();
 			}	
 			query = query.replace(/ /g, '+');
