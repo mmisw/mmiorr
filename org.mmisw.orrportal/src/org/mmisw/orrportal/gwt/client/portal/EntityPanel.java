20c708e206dbb893ef7f94942f613dc40e96f552
Carlos Rueda <carueda@gmail.com>
2012-03-31 18:58:14 +0000

    EntityPanel: Added some padding for easier legibility (OntologyTable-row style)

--- org.mmisw.orrportal/src/org/mmisw/orrportal/gwt/client/portal/EntityPanel.java
+++ org.mmisw.orrportal/src/org/mmisw/orrportal/gwt/client/portal/EntityPanel.java
@@ -91,10 +91,10 @@ public class EntityPanel extends VerticalPanel {
 		List<PropValue> props = entity.getProps();
 		for ( PropValue pv : props ) {
 			
-            cf.setStyleName(row, 0, "OntologyTable-row");
-            cf.setStyleName(row, 1, "OntologyTable-row");
+			cf.setStyleName(row, 0, "OntologyTable-row");
+			cf.setStyleName(row, 1, "OntologyTable-row");
 
-            String htmlStr;
+			String htmlStr;
 			
 			// column 0
 			String propName = pv.getPropName();
