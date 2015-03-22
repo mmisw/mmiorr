f1cffbd15a0170d9277b3c7e8b0b16dd67d9905a
Carlos Rueda <carueda@mbari.org>
2015-03-22 11:56:03 -0700

    remove obsolete method

--- org.mmisw.ont/src/org/mmisw/ont/db/Db.java
+++ org.mmisw.ont/src/org/mmisw/ont/db/Db.java
@@ -29,45 +29,45 @@ import org.mmisw.ont.util.OntUtil;
 
 /**
  * A (singleton) helper to work with the aquaportal database.
- * 
+ *
  * <p>
  * Note: This class is effectively a singleton as it is only instantiated once by {@link OntServlet}
  * (ie., the singleton-ness is not forced here).
- * 
+ *
  * <p>
  * Thread-safety: This class is not strictly thread-safe, but it is "effectively thread-safe"
- * in conjunction with {@link OntServlet} and other callers. 
- * 
+ * in conjunction with {@link OntServlet} and other callers.
+ *
  * @author Carlos Rueda
  */
 @ThreadSafe
 public class Db {
-	
+
 	private final Log log = LogFactory.getLog(Db.class);
-	
+
 
 	// obtained from the config in the init() method
-	private String aquaportalDatasource; 
-	
+	private String aquaportalDatasource;
+
 	// obtained in the init() method
 	private DataSource dataSource;
-	
 
-	/** 
-	 * Creates the instance of this helper. 
+
+	/**
+	 * Creates the instance of this helper.
 	 * Call {@link #init()} to initialize it.
 	 * @param ontConfig Used at initialization.
 	 */
 	public Db(OntConfig ontConfig) {
 	}
-	
-	/** 
+
+	/**
 	 * Required initialization.
 	 */
 	public void init() throws Exception {
 		log.info("init called.");
-		aquaportalDatasource = OntConfig.Prop.AQUAPORTAL_DATASOURCE.getValue(); 
-		
+		aquaportalDatasource = OntConfig.Prop.AQUAPORTAL_DATASOURCE.getValue();
+
 		Context initialContext = null;
 		try {
 			initialContext = new InitialContext();
@@ -79,7 +79,7 @@ public class Db {
 		catch ( NamingException ex ) {
 			throw new Exception("Failed to lookup datasource: " +aquaportalDatasource, ex);
 		}
-		
+
 		if ( initialContext != null ) {
 			// release context resources:
 			try {
@@ -90,10 +90,10 @@ public class Db {
 		}
 
 	}
-	
+
 	/**
 	 * Establishes a connection to the aquaportal datasource.
-	 * 
+	 *
 	 * @return
 	 * @throws SQLException
 	 */
@@ -101,13 +101,13 @@ public class Db {
 		Connection result = dataSource.getConnection();
 		return result;
     }
-	
+
 	/**
 	 * Calls stmt.close() if stmt is not null.
-	 * Calls connection.close() if the connection is not not null and is not already closed. 
-	 * Any {@link SQLException} during the above operations is logged as 
+	 * Calls connection.close() if the connection is not not null and is not already closed.
+	 * Any {@link SQLException} during the above operations is logged as
 	 * a warning and absorbed.
-	 *  
+	 *
 	 * @param stmt
 	 *          The statement to close. Can be null.
 	 * @param connection
@@ -122,7 +122,7 @@ public class Db {
 				log.warn("error closing SQL statment", e);
 			}
 		}
-		
+
 		if ( connection != null ) {
 			try {
 				if ( ! connection.isClosed() ) {
@@ -134,15 +134,15 @@ public class Db {
 			}
 		}
 	}
-		
+
 	/**
 	 * Obtains basic ontology info by the given URI and version.
-	 * 
+	 *
 	 * @param ontologyUri The ontology URI as exactly stored in the database.
 	 * @param version desired version
 	 * @return
 	 * @throws ServletException
-	 * @throws SQLException 
+	 * @throws SQLException
 	 */
 	public OntologyInfo getOntologyVersion(final String ontologyUri, final String version) throws ServletException {
 		Connection _con = null;
@@ -151,7 +151,7 @@ public class Db {
 			_con = getConnection();
 			_stmt = _con.createStatement();
 
-			String query = 
+			String query =
 				"select v.id, v.ontology_id, v.file_path, f.filename " +
 				"from v_ncbo_ontology v, ncbo_ontology_file f  " +
 				"where v.id = f.ontology_version_id" +
@@ -168,7 +168,7 @@ public class Db {
 				ontology.setFilePath(rs.getString(3));
 
 				ontology.setFilename(rs.getString(4));
-				
+
 				// TODO Remove this OLD way to determine filename
 //				try {
 //					URL uri_ = new URL(ontologyUri);
@@ -178,7 +178,7 @@ public class Db {
 //					// should not occur.
 //					log.debug("should not occur.", e);
 //				}
-				
+
 				return ontology;
 			}
 		}
@@ -188,29 +188,29 @@ public class Db {
 		finally {
 			closeStatementAndConnection(_stmt, _con);
 		}
-		
+
 		return null;
 	}
-	
 
-	
+
+
 	/**
 	 * Obtains basic ontology info by the given URI.
-	 * 
+	 *
 	 * <p>
 	 * Note: As of August, 2009, because on the "re-hosting" capability, the ontology URI is
-	 * no longer unique in the database, ie., there may be multiple versions associated with the 
+	 * no longer unique in the database, ie., there may be multiple versions associated with the
 	 * same ontology URI. Now, this method returns the most recent version associated
 	 * with the given URI.
-	 * 
+	 *
 	 * <p>
 	 * This uses the ontologyUri given. To try other file extensions,
 	 * use {@link #getOntologyWithExts(MmiUri, String[])}.
-	 * 
+	 *
 	 * @param ontologyUri The ontology URI as exactly stored in the database.
 	 * @return
 	 * @throws ServletException
-	 * @throws SQLException 
+	 * @throws SQLException
 	 */
 	public OntologyInfo getOntology(String ontologyUri) throws ServletException {
 		Connection _con = null;
@@ -219,7 +219,7 @@ public class Db {
 			_con = getConnection();
 			_stmt = _con.createStatement();
 
-			String query = 
+			String query =
 				"select v.id, v.ontology_id, v.file_path, f.filename " +
 				"from v_ncbo_ontology v, ncbo_ontology_file f  " +
 				"where v.id = f.ontology_version_id" +
@@ -229,7 +229,7 @@ public class Db {
 			if ( log.isDebugEnabled() ) {
 				log.debug("Executing query: " +query);
 			}
-			
+
 			ResultSet rs = _stmt.executeQuery(query);
 
 			if ( rs.next() ) {
@@ -237,9 +237,9 @@ public class Db {
 				ontology.setId(rs.getString(1));
 				ontology.setOntologyId(rs.getString(2));
 				ontology.setFilePath(rs.getString(3));
-				
+
 				ontology.setFilename(rs.getString(4));
-				
+
 				ontology.setUri(ontologyUri);
 
 				return ontology;
@@ -251,20 +251,20 @@ public class Db {
 		finally {
 			closeStatementAndConnection(_stmt, _con);
 		}
-		
+
 		return null;
 	}
-	
-	
+
+
 	/**
 	 * Gets an ontology by trying the original ontology URI, and then, if that fails,
 	 * with the file extensions "", ".owl", and ".rdf" in sequence until successful
 	 * or returning null if none of these tries works.
-	 * 
+	 *
 	 * @param mmiUri
-	 * @param foundUri If not null, the URI that was success is stored at foundUri[0]. 
-	 * 
-	 * @throws ServletException 
+	 * @param foundUri If not null, the URI that was success is stored at foundUri[0].
+	 *
+	 * @throws ServletException
 	 */
 	public OntologyInfo getOntologyWithExts(MmiUri mmiUri, String[] foundUri) throws ServletException {
 		// try with given URI:
@@ -279,11 +279,11 @@ public class Db {
 			}
 			return ontology;
 		}
-		
+
 		// TODO Remove the following trials with various extensions as we are NOT storing
 		// URIs with extension in the database, AND mmiUri.getOntologyUri() above is always
 		// with NO extension.
-		
+
 		// try with a different extension, including no extension:
 		String[] exts = { "", ".owl", ".rdf" };
 		String topicExt = mmiUri.getExtension();
@@ -304,42 +304,42 @@ public class Db {
 		}
 
 		return ontology;
-	}	
-	
+	}
+
 
 	/**
 	 * Gets the list of versions of a given ontology according to the corresponding
 	 * mmiUri identification, which is used to create the query:
 	 * <ul>
-	 *    <ul> Use wildcard "%" for the version 
-	 *    
+	 *    <ul> Use wildcard "%" for the version
+	 *
 	 *    <ul> If allExts is true, search for topic with NO extension, but also allow
 	 *       the same topic with any extension ".%" (Note: the dot is important to
 	 *       avoid getting topics that have the topic in question as suffix).
 	 * </ul>
-	 * 
+	 *
 	 * <p>
 	 * Note that the term component in the given URI is ignored.
-	 * 
+	 *
 	 * <p>
 	 * The elements are sorted such that the first element is the most recent version
 	 * available.
-	 * 
+	 *
 	 * @param mmiUri the base URI to create the version wilcard.
-	 * 
+	 *
 	 * @return list of ontologies with exactly the same given mmiUri except for the
 	 *          version component.
-	 *          
+	 *
 	 * @throws ServletException
 	 */
 	public List<OntologyInfo> getOntologyVersions(MmiUri mmiUri) throws ServletException {
-		
+
 		List<OntologyInfo> onts = new ArrayList<OntologyInfo>();
-		
+
 		if ( mmiUri.getVersion() != null && log.isDebugEnabled() ) {
 			log.debug("getOntologyVersions: " +mmiUri.getVersion()+ ": version component will be ignored.");
 		}
-		
+
 		// get ontologyUriPattern to do the "like" query:
 		String ontologyUriPattern;
 		try {
@@ -352,12 +352,12 @@ public class Db {
 			log.debug("should not occur.", e);
 			return onts;
 		}
-		
+
 		// to be added to the condition is the query string below:
 		// Add an "or" condition to allow extensions, ".%":
 		// TODO remove this ".%" condition as we are no storing URIs with extensions in the database.
 		String or_with_dot_ext = "or v.urn like '" +ontologyUriPattern+ ".%' ";
-		
+
 		// ok, now run the "like" query
 		Connection _con = null;
 		Statement _stmt = null;
@@ -365,7 +365,7 @@ public class Db {
 			_con = getConnection();
 			_stmt = _con.createStatement();
 
-			String query = 
+			String query =
 				"select v.id, v.ontology_id, v.file_path, v.urn, f.filename " +
 				"from v_ncbo_ontology v, ncbo_ontology_file f " +
 				"where v.id = f.ontology_version_id " +
@@ -377,7 +377,7 @@ public class Db {
 			if ( log.isDebugEnabled() ) {
 				log.debug("Versions query: " +query);
 			}
-			
+
 			ResultSet rs = _stmt.executeQuery(query);
 
 			while ( rs.next() ) {
@@ -385,13 +385,13 @@ public class Db {
 				ontology.setId(rs.getString(1));
 				ontology.setOntologyId(rs.getString(2));
 				ontology.setFilePath(rs.getString(3));
-				
+
 				String ontologyUri = rs.getString(4);
 				ontology.setUri(ontologyUri);
 
 				ontology.setFilename(rs.getString(5));
 				onts.add(ontology);
-				
+
 				// TODO Remove this OLD way to determine filename
 //				try {
 //					URL uri_ = new URL(ontologyUri);
@@ -410,12 +410,12 @@ public class Db {
 		finally {
 			closeStatementAndConnection(_stmt, _con);
 		}
-		
+
 		return onts;
 	}
-	
-	
-	
+
+
+
 	public OntologyInfo getMostRecentOntologyVersion(MmiUri mmiUri) throws ServletException {
 		List<OntologyInfo> onts = getOntologyVersions(mmiUri);
 		if ( onts.size() == 0 ) {
@@ -424,11 +424,11 @@ public class Db {
 		OntologyInfo ont = onts.get(0);
 		return ont;
 	}
-	
-	
+
+
 	/**
 	 * Gets the latest version of a registered ontology
-	 * 
+	 *
 	 * @param potentialOntUri. The URI that will be used to try to find a corresponding registered
 	 *                     ontology. If this is an "ont resolvable" uri, any explicit version is
 	 *                     ignored.
@@ -453,73 +453,25 @@ public class Db {
 		else {
 			ontology = getOntology(potentialOntUri);
 		}
-		
+
 		return ontology;
 	}
 
 
-	/**
-	 * Returns the list of all ontologies in the database.
-	 * 
-	 * @return
-	 * @throws ServletException
-	 * @deprecated Use {@link #getAllOntologies(boolean)}
-	 */
-	// TODO Remove this deprecated method
-	@Deprecated
-	List<OntologyInfo> getOntologies() throws ServletException {
-		List<OntologyInfo> onts = new ArrayList<OntologyInfo>();
-		Connection _con = null;
-		Statement _stmt = null;
-		try {
-			_con = getConnection();
-			_stmt = _con.createStatement();
-
-			// NOTE:
-			//    v_ncbo_ontology.id  ==  ncbo_ontology_file.ontology_version_id
-			//
-			String query = 
-				"select v.id, v.ontology_id, v.file_path, f.filename, v.urn " +
-				"from v_ncbo_ontology v, ncbo_ontology_file f " +
-				"where v.id = f.ontology_version_id";
-			
-			ResultSet rs = _stmt.executeQuery(query);
-			
-	        while ( rs.next() ) {
-	        	OntologyInfo ontology = new OntologyInfo();
-	        	ontology.setId(rs.getString(1));
-	        	ontology.setOntologyId(rs.getString(2));
-	        	ontology.setFilePath(rs.getString(3));
-	        	ontology.setFilename(rs.getString(4));
-	        	ontology.setUri(rs.getString(5));
-	        	onts.add(ontology);
-	        }
-		}
-		catch (SQLException e) {
-			throw new ServletException(e);
-		}
-		finally {
-			closeStatementAndConnection(_stmt, _con);
-		}
-	
-		return onts;
-	}
-
-	
-	/**
+    /**
 	 * Gets the list of registered ontologies.
 	 * Always with URIs in *versioned* form for the full-hosted entries.
-	 * 
+	 *
 	 * @param allVersions true to report ALL versions of all ontologies;
 	 *                    false to return just the lastest versions
 	 * @return
 	 * @throws ServletException
 	 */
 	public List<OntologyInfo> getAllOntologies(boolean allVersions) throws ServletException {
-		
+
 		List<OntologyInfo> onts = new ArrayList<OntologyInfo>();
 		Map<String,OntologyInfo> mostRecent = new LinkedHashMap<String,OntologyInfo>();
-		
+
 		// If allVersions==true, we add to onts immediately; otherwise
 		// we keep track of the most recent ontology for each ontology_id in mostRecent.
 		// See below.
@@ -534,7 +486,7 @@ public class Db {
 			_stmt = _con.createStatement();
 
 			// note that this is sorted in increasing ontology_id,version_number
-			String query = 
+			String query =
 				"select v.id, v.ontology_id, v.file_path, f.filename, v.urn, v.display_label, v.contact_name, v.version_number," +
                 "       v.version_status " +
 				"from v_ncbo_ontology v, ncbo_ontology_file f " +
@@ -542,7 +494,7 @@ public class Db {
 				"order by v.ontology_id, v.version_number";
 
 			ResultSet rs = _stmt.executeQuery(query);
-			
+
 	        while ( rs.next() ) {
 
 	        	OntologyInfo ontology = new OntologyInfo();
@@ -589,7 +541,7 @@ public class Db {
 	        	}
 	        	return onts;
 	        }
-		} 
+		}
 		catch (SQLException e) {
 			throw new ServletException(e);
 		}
@@ -597,7 +549,7 @@ public class Db {
 			closeStatementAndConnection(_stmt, _con);
 		}
 	}
-	
+
 	/**
 	 * Gets info about a user.
 	 * @param username
@@ -610,24 +562,24 @@ public class Db {
 			_con = getConnection();
 			_stmt = _con.createStatement();
 
-			String query = 
+			String query =
 				"select id, username, email, firstname, lastname, phone, date_created " +
 				"from ncbo_user " +
 				"where username = '" +username+ "'";
-			
+
 			ResultSet rs = _stmt.executeQuery(query);
-			
+
 			if ( rs.next() ) {
 				ResultSetMetaData md = rs.getMetaData();
 				Map<String,String> props = new LinkedHashMap<String,String>();
-			
+
 				for ( int i = 1, count = md.getColumnCount(); i <= count; i++ ) {
 					String value = rs.getString(i);
 					if ( value != null ) {
 						props.put(md.getColumnName(i), value);
 					}
 				}
-				
+
 	        	return props;
 	        }
 	        else {
@@ -641,7 +593,7 @@ public class Db {
 			closeStatementAndConnection(_stmt, _con);
 		}
 	}
-	
+
 	/**
 	 * Gets all registered users.
 	 * @return
@@ -649,7 +601,7 @@ public class Db {
 	public List<Map<String,String>> getAllUserInfos() throws ServletException {
 		// to format date_created appropriately (xsd dateTime)
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
-		
+
 		List<Map<String,String>> list = new ArrayList<Map<String,String>>();
 		Connection _con = null;
 		Statement _stmt = null;
@@ -657,34 +609,34 @@ public class Db {
 			_con = getConnection();
 			_stmt = _con.createStatement();
 
-			String query = 
+			String query =
 				"select id, username, email, firstname, lastname, phone, date_created " +
 				"from ncbo_user " +
 				"order by date_created " ;
-			
+
 			ResultSet rs = _stmt.executeQuery(query);
-			
+
 			while ( rs.next() ) {
 				ResultSetMetaData md = rs.getMetaData();
 				Map<String,String> props = new LinkedHashMap<String,String>();
-			
+
 				for ( int i = 1, count = md.getColumnCount(); i <= count; i++ ) {
 					String value = rs.getString(i);
 					if ( value != null ) {
 						String colName = md.getColumnName(i);
-						
+
 						if ( colName.equals("date_created") ) {
 							Timestamp date_created = rs.getTimestamp(i);
 							value = sdf.format(date_created);
 						}
-						
+
 						props.put(colName , value);
 					}
 				}
-				
+
 	        	list.add(props);
 	        }
-			
+
 			return list;
 		}
 		catch (SQLException e) {
